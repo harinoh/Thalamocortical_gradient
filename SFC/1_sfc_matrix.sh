f5d64b596065a1c8b5fb35b5e240caa410bbf125
harinoh <harinoh818@gmail.com>
2025-07-22 21:42:13 +0900

    Delete SFC directory

--- SFC/1_sfc_matrix.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-
-MATLAB="/usr/local/MATLAB/R2020a/bin/matlab"
-dir=/Volume/CCNC/harin_oh/1_thalamocortical/code/SFC
-
-$MATLAB -nodisplay -nosplash -nodesktop -r "run('$dir/sfc_4_2.m');exit;"
--- SFC/2_dc_matrix.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-
-MATLAB="/usr/local/MATLAB/R2020a/bin/matlab"
-dir=/Volume/CCNC/harin_oh/1_thalamocortical/code/SFC
-
-$MATLAB -nodisplay -nosplash -nodesktop -r "run('$dir/degree_sfc_4_3.m')/;exit;"
