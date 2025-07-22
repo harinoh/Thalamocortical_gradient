6db849fe2c608d10f4d0dd0fb8300a396c2094e4
harinoh <harinoh818@gmail.com>
2025-07-22 21:41:05 +0900

    Update 1_sfc_matrix.sh

--- SFC/1_sfc_matrix.sh
+++ SFC/1_sfc_matrix.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
 MATLAB="/usr/local/MATLAB/R2020a/bin/matlab"
-dir=/Volume/CCNC_T_4/harin_oh/1_thalamocortical/code/SFC_obesity
+dir=/Volume/CCNC/harin_oh/1_thalamocortical/code/SFC
 
 $MATLAB -nodisplay -nosplash -nodesktop -r "run('$dir/sfc_4_2.m');exit;"
